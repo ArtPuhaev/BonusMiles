ecd5248bc7757f7835c2b00ce0d191f5867d4f67
ArturPuhaev <artpuhaev@gmail.com>
2025-02-28 22:01:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 12000;
+        int BonusMile = 20;
+        int Miles = price/BonusMile;
+        System.out.println("Bonus Miles: " + Miles);
+    }
+}
